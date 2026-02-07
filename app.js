e9ab3b1a1ad0cc66c0976a50a1c2e38dfddc0aba
raphaeldebot <debot.raphael@gmail.com>
2026-02-07 22:42:28 +0100

    ajout de 3 fonction pour que le rond se dessinne tout du long ou je clique un peu comme un brush

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ const canvas = document.getElementById("kaili-brush");
 const ctx = canvas.getContext("2d");
 
 
-ctx.beginPath();
-ctx.arc(200,150,10,0,Math.PI*2);
-ctx.fill();
+let drawing = false;
+
+canvas.addEventListener("pointerdown", () => {
+    drawing = true;
+});
+
+canvas.addEventListener("pointerup", () => {
+    drawing = false;
+});
+
+
+canvas.addEventListener("pointermove", (e) => {
+    if (!drawing) return;
+
+    const x = e.offsetX;
+    const y = e.offsetY;
+
+
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.fill();
+});
+
+
