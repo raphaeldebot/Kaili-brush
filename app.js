cb93b81662fe154e71bc60afd5ddb931a1a72fab
raphaeldebot <debot.raphael@gmail.com>
2026-02-08 01:00:36 +0100

    ajout d'un slide pour le choix de la couleur du brush

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const canvas = document.getElementById("kaili-brush");
 const brushSizeInput = document.getElementById("brushSize");
+const brushColorInput = document.getElementById("brushColor")
 const ctx = canvas.getContext("2d");
 
 /* Ma fonction pour mon brush de base*/
 let drawing = false;
 let lastX = 0;
+
 let lastY = 0;
+/* Ma fonction pour le slider de mise a jour de la taille du brush */
+let brushSize = Number(brushSizeInput.value);
+
+brushSizeInput.addEventListener("input", () => {
+    brushSize = Number(brushSizeInput.value);
+});
+
+/* Ma fonction pour le slider de choix de la couleur du brush */
+let brushColor = brushColorInput.value;
+
+brushColorInput.addEventListener("input", () => {
+    brushColor = brushColorInput.value;
+})
+
+
+
 
 canvas.addEventListener("pointerdown", (e) => {
     drawing = true;
@@ -32,13 +50,8 @@ canvas.addEventListener("pointermove", (e) => {
     ctx.lineWidth = brushSize;
     ctx.lineCap = "round";
     ctx.stroke();
+    ctx.strokeStyle = brushColor;
 
     lastX = x;
     lastY = y;    
-});
-/* Ma fonction pour le slider de mise a jour de la taille du brush */
-let brushSize = brushSizeInput.value;
-
-brushSizeInput.addEventListener("input", () => {
-    brushSize = brushSizeInput.value;
 });
